957260305cf59573c60b3dd5abe4f50aedeb884b
Christina Bernhardt <sozia76@web.de>
2024-01-18 19:51:00 +0100

    questions

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Prince Charles",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie viele Weihnachtsbäume werden in Deutschland pro Jahr verkauft?",
+        "answer_1": "1 Million",
+        "answer_2": "3 Millionen",
+        "answer_3": "10 Millionen",
+        "answer_4": "30 Millionen",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie viele Liter Bier werden in Deutschland pro Kopf jährlich getrunken?",
+        "answer_1": "10 Liter",
+        "answer_2": "100 Liter",
+        "answer_3": "1000 Liter",
+        "answer_4": "10000 Liter",
+        "right_answer": 2
+    },
+    {
+        "question": "Wie lang ist der Begattungsapparat der 15 cm langen Bananenschnecke?",
+        "answer_1": "2 cm",
+        "answer_2": "8 cm",
+        "answer_3": "20 cm",
+        "answer_4": "80 cm",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie viele Einkerbungen hat ein Golfball?",
+        "answer_1": "33",
+        "answer_2": "36",
+        "answer_3": "333",
+        "answer_4": "336",
+        "right_answer": 4
+    }
+];
