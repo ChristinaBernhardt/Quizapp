7b8d6fc0e023739ca65b1f536a775508cf4efb7f
Christina Bernhardt <sozia76@web.de>
2024-01-19 21:30:02 +0100

    console.log right answer

--- script.js
+++ script.js
@@ -1,42 +1,91 @@
 let questions = [
     {
         "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Prince Charles",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
+        "answer1": "Robbie Williams",
+        "answer2": "Prince Charles",
+        "answer3": "Tim Berners-Lee",
+        "answer4": "Justin Bieber",
         "right_answer": 3
     },
     {
         "question": "Wie viele Weihnachtsbäume werden in Deutschland pro Jahr verkauft?",
-        "answer_1": "1 Million",
-        "answer_2": "3 Millionen",
-        "answer_3": "10 Millionen",
-        "answer_4": "30 Millionen",
+        "answer1": "1 Million",
+        "answer2": "3 Millionen",
+        "answer3": "10 Millionen",
+        "answer4": "30 Millionen",
         "right_answer": 4
     },
     {
         "question": "Wie viele Liter Bier werden in Deutschland pro Kopf jährlich getrunken?",
-        "answer_1": "10 Liter",
-        "answer_2": "100 Liter",
-        "answer_3": "1000 Liter",
-        "answer_4": "10000 Liter",
+        "answer1": "10 Liter",
+        "answer2": "100 Liter",
+        "answer3": "1000 Liter",
+        "answer4": "10000 Liter",
         "right_answer": 2
     },
     {
         "question": "Wie lang ist der Begattungsapparat der 15 cm langen Bananenschnecke?",
-        "answer_1": "2 cm",
-        "answer_2": "8 cm",
-        "answer_3": "20 cm",
-        "answer_4": "80 cm",
+        "answer1": "2 cm",
+        "answer2": "8 cm",
+        "answer3": "20 cm",
+        "answer4": "80 cm",
         "right_answer": 4
     },
     {
         "question": "Wie viele Einkerbungen hat ein Golfball?",
-        "answer_1": "33",
-        "answer_2": "36",
-        "answer_3": "333",
-        "answer_4": "336",
+        "answer1": "33",
+        "answer2": "36",
+        "answer3": "333",
+        "answer4": "336",
+        "right_answer": 4
+    },
+    {
+        "question": "Wie viele Einkerbungen hat ein Golfball?",
+        "answer1": "33",
+        "answer2": "36",
+        "answer3": "333",
+        "answer4": "336",
         "right_answer": 4
     }
+
 ];
+
+let currentQuestion = 2;
+
+function init(){
+       document.getElementById('counter').innerHTML=questions.length;
+       showQuestion();
+}
+
+function showQuestion(){
+let question = questions[currentQuestion];
+document.getElementById('questionText').innerHTML = question['question'];
+document.getElementById('answer1').innerHTML = question['answer1'];
+document.getElementById('answer2').innerHTML = question['answer2'];
+document.getElementById('answer3').innerHTML = question['answer3'];
+document.getElementById('answer4').innerHTML = question['answer4'];
+}
+
+function answer(selection){
+    let question = questions[currentQuestion];
+    console.log('selected answer is ', selection);
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('right answer is ', question['right_answer']);
+    console.log('selected Question Number ', selectedQuestionNumber);
+    if (selectedQuestionNumber == question['right_answer']) {
+        console.log('geil!!!');        
+    }
+    else {
+        console.log('fail'); 
+
+    }
+// if (selection === questions[currentQuestion] === [right_answer]) {
+//     console.log('right answer ', selection);
+    // document.getElementById(selection).classList.add("showgreen");
+    
+// }
+// else
+// {
+    // document.getElementById(`likeicon-red${index}`).classList.add("showred");
+// }
+}
